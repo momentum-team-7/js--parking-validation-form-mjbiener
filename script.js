ccff6819d727c82268c64881e4f964ec2f7f6bf8
mjbiener <michaelbiener@yahoo.com>
2021-01-31 17:08:24 -0500

    working on 6-7 skipped 5 for now

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ let formIsValid
 
 window.addEventListener('submit', event => {
     event.preventDefault()
-    confirmValidForm
+   
 })
 
+form.addEventListener('submit', validate)
+ 
 
-// dayofweek();
-
-
-form.addEventListener('click', validate)
 
 function validate(event) {
-    formIsValid = true
-    confirmValidForm()
+    event.preventDefault();
+    if (formIsValid = true) {
+    confirmValidForm();
+    validateCardNumber();
+    validateExpDate();
+    validateCarYear();
+    validateParkDate();
+    totalExpense();
+   }  
 }
 
 
@@ -29,28 +34,137 @@ function confirmValidForm() {
 }
 
 
+function validateCardNumber () {
+    let cardInput= document.querySelector("#credit-card")
+    let cardValue= cardInput.value
+    let parentEl= cardInput.parentElement
+
+    document.getElementsByTagName("label")[4].setAttribute("id", "card-label");
+    let cardLabel = document.querySelector("card-label")
+    
+    if (cardValue.length < 16 || cardValue.length > 16 || isNaN(cardValue)) {
+        console.log("Card number is invalid")
+        parentEl.classList.remove("input-valid")
+        cardLabel.textContent = "Credit card number must be 16 digits"
+        parentEl.classList.add("input-invalid")
+        formIsValid = false
+    }   else {
+        console.log("Card number is valid")
+        parentEl.classList.remove("input-invalid")
+        // cardLabel.textContent = "Credit card"
+        parentEl.classList.add("input-valid")
+    }
+}
+
+function validateExpDate() {
+    let expDateInput = document.querySelector("#expiration")
+    let expValue = expDateInput.value
+    let parentEl = expDateInput.parentElement
+    
+    let today = new Date();
+    let todayMonth = today.getMonth() +1;
+    let todayYear = today.getFullYear() %100;
+
+    document.getElementsByTagName('label')[6].setAttribute("id", "exp-label");
+    let expLabel = document.querySelector("#exp-label");
+
+    let monthValidate = expValue.substring(0, 2);
+    let yearValidate = expValue.substring(3);
+
+    if (yearValidate >= todayYear && monthValidate >= todayMonth) {
+        console.log("Expiration Date is valid")
+        parentEl.classList.remove("input-invalid")
+        expLabel.textContent = "Expiration"
+        parentEl.classList.add("input-valid")
+    }   else {
+        console.log("Expiration Date is invalid")
+        parentEl.classList.remove("input-valid")
+        parentEl.classList.remove("input-valid")
+        parentEl.classList.add("input-invalid")
+        expLabel.textContent = "Expiration date must be in the future"
+        formIsValid = false
+
+    }
+
+}
+
+function validateCarYear() {
+    let carYear = document.querySelector("#car-year");
+    let carYearDate = parseInt(carYear.value);
+    let currentYear= new Date().getFullYear() + 1;
+
+     if (carYearDate > currentYear) {
+        console.log("Car year is invalid")
+        formIsValid = false;
+        carYear.setCustomValidity('Car year is invalid');
+
+    } else { 
+        console.log("Car year is valid")
+        carYear.setCustomValidity("");
+    }
+}   
+
+function validateParkDate(){
+    let parkDateInput = document.querySelector("#start-date")
+    let parkDateValue = parkDateInput.value
+    let parentEl = parkDateInput.parentElement
+    let today = new Date ();
+    // let day = new Date(parkDateValue).getDay();
+    let parkDaysInput = document.querySelector ("#days")
+    let parkDaysValue = parkDaysInput.value
+    parkDateValue = new Date(parkDateValue);
+    
+    document.getElementsByTagName("label")[2].setAttribute("id", "date-label");
+    let dateLabel = document.querySelector("#date-label")
+
+    if (parkDateValue > today) {
+        console.log("parking date is valid")
+        dateLabel.textContent = "Date parking"
+        parentEl.classList.add("input-valid")
+    } else {
+        console.log("parking date is invalid")
+        parentEl.classList.remove("input-valid")
+        dateLabel.textContent = "Enter a future parking date"
+        parentEl.classList.add("input-invalid")
+        formisvalid = false
+    }
+}
+
 
 function totalExpense() {
     let price = 5
     const totaldays = document.querySelector('#days').value
     let fullPrice = totaldays * price
-    return `Your total cost is $${fullPrice}`
-    console.log (fullPrice)
+    console.log (fullPrice);
+
+    return `Your total cost is $${fullPrice}`;
 }
 
 
+ 
+// #5 code
 
 
 
 
-function formatDate (date){
-    let startDate = document.querySelector('#start-date').value;
-    let actualDate = startDate.replace(/-/g, '\/');
-    console.log (actualDate)
-}
+
+
+
+
+
+
+
+
+// Trying to figure out how to make # 5 work
+
+// function formatDate (date){
+//     let startDate = document.querySelector('#start-date').value;
+//     let actualDate = startDate.replace(/-/g, '/');
+//     console.log (actualDate)
+// }
 
 // function changeDate(date) {
-//     let newDate = date.replace(/-/g, '\/');
+    // let newDate = date.replace(/-/g, '\/');
 //     console.log (newDate);
 //     return newDate
 // }
@@ -60,38 +174,16 @@ function formatDate (date){
 // 
 
 
-function dayofweek(startDate) {
-    let actualDate = startDate.replace(/-/g, '\/');
-    let d = new Date(actualDate).dayofweek();
-    console.log(d);
+// function dayofweek(startDate) {
+//     let startDate = document.querySelector('#start-date').value
+//     let actualDate = startDate.replace ( /-/g , '/');
+//     let d = new Date(actualDate).dayofweek();
+//     console.log(d);
 
-    return d;
-}
+//     return d;
+// }
 
 
 
 
-//below code copied from assignment notes //
-
-function validateCardNumber(number) {
-    var regex = new RegExp("^[0-9]{16}$");
-    if (!regex.test(number))
-        return false;
-
-    return luhnCheck(number);
-}
-
-function luhnCheck(val) {
-    var sum = 0;
-    for (var i = 0; i < val.length; i++) {
-        var intVal = parseInt(val.substr(i, 1));
-        if (i % 2 == 0) {
-            intVal *= 2;
-            if (intVal > 9) {
-                intVal = 1 + (intVal % 10);
-            }
-        }
-        sum += intVal;
-    }
-    return (sum % 10) == 0;
-}``
+//below code copied from assignment notes for # 6//
